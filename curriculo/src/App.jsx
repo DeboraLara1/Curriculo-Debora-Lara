8c7dedf3a63ec3699d0293096f06e6b7bc1cc98a
Debora Lara <debora.roldanlara@gmail.com>
2021-10-19 16:34:01 -0300

    subindo versão completa do curriculo

---
+++ curriculo/src/App.jsx
@@ -0,0 +1,120 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import Styled from './Styled.jsx';
+import { FiPhone } from "react-icons/fi";
+import {AiOutlineMail, AiFillLinkedin, AiFillGithub, AiOutlineFundProjectionScreen} from 'react-icons/ai';
+import { BiMinus, BiRocket } from 'react-icons/bi';
+import { ImBook, ImBubbles3, ImList2 } from 'react-icons/im';
+import { GoLocation } from 'react-icons/go';
+import Debora from './img/eu.jpg';
+
+export default () => {
+  return (
+    <>
+      <Styled.Contaneir>
+        <Styled.Pessoal>
+          <Styled.Teste>
+            <Styled.IMG src={Debora} alt="Auto descrição: Mulher, branca, olhos verdes, loira, cabelos compridos e com franja" />
+          </Styled.Teste>
+          <Styled.Div>
+            <Styled.H1> Debora Lara </Styled.H1>
+            <Styled.P marginTop> Desenvolvedora Front End </Styled.P>
+          </Styled.Div>
+
+          <Styled.Perfil borderLeftStyle> Perfil </Styled.Perfil>
+          <Styled.P marginLeft fontSize>Desenvolvedora Front End, Angular, Angular Material, Bootstrap, React, JavaScript, Typescript, HTML, CSS. </Styled.P>
+
+          <Styled.Perfil borderLeftStyle > Contato </Styled.Perfil>
+          <Styled.P marginLeft fontSize marginBottom><FiPhone  /> 41 998605554 </Styled.P> 
+          <Styled.P  fontSize marginBottom padding><AiOutlineMail /> debora.roldanlara@gmail.com </Styled.P> 
+          <Styled.P marginLeft fontSize marginBottom><AiFillLinkedin /><a href='https://www.linkedin.com/in/deboralara/' target="_blank" rel="noreferrer">www.linkedin.com/in/deboralara</a>  </Styled.P> 
+          <Styled.P marginLeft fontSize marginBottom>< AiFillGithub /><a href='https://github.com/deboralara1' target="_blank" rel="noreferrer">https://github.com/deboralara1 </a></Styled.P>
+          <Styled.P marginLeft fontSize marginBotto>< AiOutlineFundProjectionScreen /><a href='https://deboralara1.github.io/Portfolio' target="_blank" rel="noreferrer"> https://deboralara1.github.io/Portfolio</a></Styled.P>
+          <Styled.P  fontSize padding>< GoLocation /> Colombo - PR </Styled.P>
+
+          <Styled.Perfil borderLeftStyle > Qualificações e Competências </Styled.Perfil>
+          
+            <Styled.DivCompetencias>
+              ReactJs 
+              <Styled.Input background="linear-gradient(to right, #bebaba 70%, #ffffff 30%)"/>
+            </Styled.DivCompetencias>
+            
+          <Styled.DivCompetencias> Styled Components <Styled.Input background="linear-gradient(to right, #bebaba 75%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Testes Unitários <Styled.Input background="linear-gradient(to right, #bebaba 70%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Angular <Styled.Input background="linear-gradient(to right, #bebaba 80%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Angular Material <Styled.Input background="linear-gradient(to right, #bebaba 80%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> JavaScript | TypeScript <Styled.Input background="linear-gradient(to right, #bebaba 85%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> JQuery <Styled.Input background="linear-gradient(to right, #bebaba 40%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> CSS <Styled.Input background="linear-gradient(to right, #bebaba 85%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Bootstrap <Styled.Input background="linear-gradient(to right, #bebaba 90%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> API <Styled.Input background="linear-gradient(to right, #bebaba 80%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> REST <Styled.Input background="linear-gradient(to right, #bebaba 80%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Figma <Styled.Input background="linear-gradient(to right, #bebaba 60%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Design (Adobe XD) básico <Styled.Input background="linear-gradient(to right, #bebaba 30%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> HTML <Styled.Input background="linear-gradient(to right, #bebaba 90%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Domínio de Lógica de Lirogramação <Styled.Input background="linear-gradient(to right, #bebaba 90%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Git, GitHub, Gerrit, Jira, Kanban, Trello <Styled.Input background="linear-gradient(to right, #bebaba 80%, #ffffff 30%)"/></Styled.DivCompetencias>
+          <Styled.DivCompetencias> Windows e Linux intermediário <Styled.Input background="linear-gradient(to right, #bebaba 70%, #ffffff 30%)"/></Styled.DivCompetencias>
+
+        </Styled.Pessoal>
+
+        <Styled.Section>
+          <Styled.Main>
+            <Styled.Perfil><BiRocket /> Experiencias Profissionais </Styled.Perfil>
+            <Styled.H6 borderLeftStyle> ESTÁGIO DE DESENVOLVEDORA FRONT-END REACT| ZUP INNOVATION | 04/2021 </Styled.H6>
+            <Styled.P marginLeft paddingLeft fontSize><BiMinus  /> Atuei em uma trilha de conhecimento em que consistia estudar e aplicar as principais tecnologias utilizadas nos projetos da empresa, para mensuração do conhecimento e preparação para atuação em projetos da empresa. </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Atuo no desenvolvimento de projetos desenvolvendo aplicações para realizar testes em novos produtos da empresa, atuando no Front-End utilizando HTML, CSS, React, Styled Components, JavaScript,Testes Unitários, TypeScript e trabalhando constantemente com integrações à APIs com REST. </Styled.P> 
+            <Styled.H6 borderLeftStyle> ESTÁGIO DE DESENVOLVEDORA FRONT-END ANGULAR | ALTA RAIL TECHNOLOGY – ART | 03/2020 À 12/2020 </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Atuei no desenvolvimento de sistemas próprios da empresa que permitem o gerenciamento das linhas rodoferroviárias, atuando no Front-End dos sistemas utilizando HTML, CSS, Angular, TypeScript e trabalhando constantemente com integrações à APIs com REST. </Styled.P> 
+
+            <Styled.Perfil><ImBubbles3 /> Idiomas </Styled.Perfil>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Espanhol = Nativo </Styled.P>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Português: Fluente </Styled.P>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Inglês: básico </Styled.P>
+
+            <Styled.Perfil><ImBook /> Educação </Styled.Perfil>
+            <Styled.H6 borderLeftStyle> ENSINO SUPERIOR | 07/2019 A 12/ 2021 | CENTRO UNIVERSITÁRIO DE MARINGÁ - UNICESUMAR </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Tecnólogo em Análise e Desenvolvimento de Sistemas. </Styled.P>
+
+            
+            <Styled.Perfil><ImList2 /> Resumo de Qualificações </Styled.Perfil>
+            <Styled.H6 borderLeftStyle> DO ZERO AO UM | PROJETO MAITHA TECH </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> ReactJS </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> TypeScript </Styled.P> 
+
+            <Styled.H6 borderLeftStyle> REACTJS NINJA | UDEMY </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> 5 meses </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Projeto de mentoria no Front End (Html, CSS, Javascript e React) </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Rest </Styled.P> 
+
+            <Styled.H6 borderLeftStyle> ANGULAR & TYPESCRIPT | LOIANE </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Angular </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> TypeScript </Styled.P> 
+
+            <Styled.H6 borderLeftStyle> WEB-DESING & RONT-END| ORIGAMID </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> HTML, CSS </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> JavaScript, JQuery </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Design (Adobe XD) </Styled.P> 
+
+            <Styled.H6 borderLeftStyle> CURSO EM VÍDEO | YOUTUBE </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Lógica de programação </Styled.P> 
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> JavaScript </Styled.P> 
+
+            <Styled.H6 borderLeftStyle> GIT E GITHUB | UDEMY </Styled.H6>
+            <Styled.P marginLeft fontSize paddingLeft><BiMinus  /> Git e Github para iniciantes </Styled.P> 
+
+
+
+
+
+
+          </Styled.Main>
+        </Styled.Section>
+
+      </Styled.Contaneir>
+
+        
+        
+    </>
+  );
+}
---
+++ curriculo/src/Styled.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable import/no-anonymous-default-export */
+import styled from 'styled-components';
+
+const Contaneir = styled.div`
+    
+  min-height: 100vh;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  @media screen and (max-width:1200px) {
+  
+    grid-template-columns: repeat(1, 1fr);
+  }
+`
+const Pessoal = styled.div`
+  grid-column: span 2;
+  height: 100%;
+  color: #ffffff;
+  background-color: #006a7a;
+  a {
+    color: #ffffff;
+    text-decoration: none;
+    padding: 2%;
+  }
+  @media screen and (max-width:780px) {
+    grid-column: span 3;
+  
+  }
+`
+
+const Section = styled.div`
+  grid-column: span 5;
+  background-color: #f1f1f1;
+  height: 100%;
+  @media screen and (max-width:780px) {
+    grid-column: span 4;
+  }
+`
+
+const Main = styled.div`
+  color: #006a7a;
+  padding: 2rem 3rem;
+  padding-bottom: 2rem;
+`
+
+const Teste =styled.div`
+  margin-top: 15px;
+  align-items: center;
+  justify-content: space-around;
+  display: flex;
+`
+
+const IMG = styled.img`
+  width: 136px;
+  height: 156px;
+  border-radius: 50%;
+`
+
+const H1 = styled.h1`
+  margin-bottom: 1px;
+`
+
+const P = styled.p`
+  margin-top: ${props => props.marginTop ? "1px" : "0px"};
+  margin-left: ${props => props.marginLeft ? "20px" : "12px"};
+  font-size: ${props => props.fontSize ? "13.3px;" : "15px"};
+  margin-bottom: ${props => props.marginBottom ? "8x;" : "0px"}; 
+  padding: ${props => props.padding ? "2%;" : "0px"};
+  padding-left: ${props => props.paddingLeft ? "2%;" : "0px"};
+
+`
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const DivCompetencias = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  align-items: center;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 14px;
+`
+
+const Li = styled.li`
+  font-size: 14px;
+  margin-left: 8%;
+  margin-top: 2px;
+`
+
+const DivLinea = styled.div`
+  display: flex;
+  position: absolute;
+  border-top:solid 2px;
+  width: ${props => props.width};
+  top: ${props => props.top};
+  left: ${props => props.left};
+`
+
+const Perfil = styled.h3`
+  margin-left: 4%;  
+  border-left-style: ${props => props.borderLeftStyle ? 'solid' : ''};
+  border-left-width: medium;
+  padding-left: 3px;
+`
+
+const H6 = styled.h6`
+  margin-left: 4%;  
+  border-left-style: ${props => props.borderLeftStyle ? 'solid' : ''};
+  border-left-width: medium;
+  padding-left: 3px;
+`
+
+const Input = styled.input`
+  background:${props => props.background};
+  border-radius: 50px;
+  border: none;
+  height: 7px;
+`
+
+export default{
+  Contaneir,
+  Teste,
+  IMG,
+  Pessoal,
+  H1,
+  P,
+  Li,
+  Div,
+  DivCompetencias,
+  DivLinea,
+  Perfil,
+  H6,
+  Section,
+  Main,
+  Input
+}
